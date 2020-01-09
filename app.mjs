9309ff1156d43b35acaeeed2d1251185c38c811c
Makon Cline <makoncline@gmail.com>
2020-01-09 13:07:35 -0600

    change to use modules

--- app.mjs
+++
@@ -1,8 +0,0 @@
-import getDaylilyData from './getDaylilyData.mjs';
-import fs from 'fs';
-
-getDaylilyData('user.csv', 'ahs.csv', (csv)=>{
-  fs.writeFile('output.csv', csv, (err)=>{
-    if (err) console.log;
-  });
-});
